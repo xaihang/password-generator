8903168be5a5aad6c3147ae74dff2b7dd745c1fb
xaihang <xaihang12@gmail.com>
2022-10-12 18:39:25 -0500

    commented on type of criteria

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+//types of inputs for users to select from 
 var loweredCased = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
 
 var upperedCased = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
@@ -8,6 +11,7 @@ var upperedCased = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z';
 var specialCharaters = '!,@,#,$,%,^,&,*,(,),_,-,+,=,?,>,<';
 
 var numbers = '0,1,2,3,4,5,6,7,8,9';
+
 
 
 //assignment criterias:
@@ -18,7 +22,7 @@ var numbers = '0,1,2,3,4,5,6,7,8,9';
 // 3. generate password based on user's allowed/selected criteria
 // 4. display password on the page 
 
-function generatePassword(){
+function generatePassword() {
   window.prompt()
 }
 
